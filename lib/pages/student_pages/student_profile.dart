4c7600503f162e0de0a8602b66e91ad08402a1be
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-12-09 23:42:59 +0530

    added individual student analytics

--- lib/pages/student_pages/student_profile.dart
+++ lib/pages/student_pages/student_profile.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:attendance_app/Utils/percent_indicator.dart';
 import 'package:attendance_app/Utils/toast.dart';
 import 'package:attendance_app/constants/routes/routes_names.dart';
+import 'package:attendance_app/data/database/students/constants.dart';
 import 'package:attendance_app/data/database/students/service.dart';
 import 'package:attendance_app/pages/common_pages/home_page.dart';
 import 'package:flutter/material.dart';
@@ -12,10 +13,15 @@ import '../../data/models/student_model/student_model.dart';
 import '../teacher_pages/attendance_page.dart';
 
 class StudentProfilePage extends StatefulWidget {
+
+
+
+
+
   final Student student;
   final StudentDBService service;
   final Class hisClass;
-  const StudentProfilePage(
+   const StudentProfilePage(
       {super.key,
       required this.student,
       required this.service,
@@ -26,6 +32,23 @@ class StudentProfilePage extends StatefulWidget {
 }
 
 class _StudentProfilePageState extends State<StudentProfilePage> {
+
+  Map<String, List<String>> data={} ;
+
+  // Define headers for your columns
+  final List<String> headers = [
+    'Student Present Dates',
+  ];
+
+  Future<void>fetchStudentAttendance()async{
+    final List<Map<String,dynamic>> dat=await widget.service.getStudentAttendanceMapList(student);//List<Map<String,dynamic>>
+    for(int i=0;i<dat.length;i++){
+      data["$i"]=[dat.elementAt(i)[dateCol]];
+    }
+    print(data);
+  }
+
+
   late final Student student;
   @override
   void initState() {
@@ -235,9 +258,72 @@ class _StudentProfilePageState extends State<StudentProfilePage> {
                               student.nOfClassesAttended / c.data!);
                         }),
 
-                    SizedBox.fromSize(
-                      size: const Size.square(80),
-                    ),
+                    SizedBox.fromSize(size: const Size(30, 30),),
+                    FutureBuilder(future: fetchStudentAttendance(), builder: (c,s){
+                      if(s.connectionState==ConnectionState.waiting) return const Center(child: CircularProgressIndicator(),);
+                      return SizedBox(
+                        height: 200,
+                        child: Expanded(
+                          // padding: const EdgeInsets.only(top: 10,bottom: 10,left: 1,right: 1),
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical, // For horizontal scrolling if the table is wide
+                            child: DataTable(
+                              columns: headers
+                                  .map(
+                                    (header) => DataColumn(
+                                  label: Text(
+                                    header,
+                                    style: const TextStyle(
+                                      fontFamily: 'Inter',
+                                      letterSpacing: 0.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              )
+                                  .toList(),
+                              rows: List<DataRow>.generate(
+                                data.length,
+                                    (int index) {
+                                  String key = index.toString(); // Get the key from the map
+                                  List<String> rowData = data[key]!; // Get the row data using the key
+                                  return DataRow(
+                                    color: WidgetStateProperty.all(
+                                      index % 2 == 0
+                                          ? Colors.grey[200] // Even rows in a different color
+                                          : Colors.white,
+                                    ),
+                                    cells: rowData
+                                        .map(
+                                          (cellData) => DataCell(
+                                        Center(
+                                          child: Text(
+                                            cellData,
+                                            style: const TextStyle(
+                                              fontFamily: 'Inter',
+                                              letterSpacing: 0.0,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                        .toList(),
+                                  );
+                                },
+                              ),
+                              headingRowHeight: 56.0, // Height of the header row
+                              dataRowMinHeight: 48.0, // Height of each data row
+                              columnSpacing: 20.0, // Space between columns
+                              headingRowColor:
+                              WidgetStateProperty.all(Colors.blue), // Header color
+                              border: TableBorder.all(color: Colors.grey, width: 1), // Table border
+                              dividerThickness: 1.0, // Divider thickness between rows
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+
                     Column(
                       mainAxisSize: MainAxisSize.max,
                       mainAxisAlignment: MainAxisAlignment.center,
